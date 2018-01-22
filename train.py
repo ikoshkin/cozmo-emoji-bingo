1d4a7c29dbc516311c44b85c18df65470c09084d
Gosha Koshkin <ihorkoshkin@gmail.com>
2018-01-22 06:24:09 -0700

    history.history

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from data import get_data_dirs
 
 def save_history(history, fname):
 
-    h_df = pd.DataFrame(history)
+    h_df = pd.DataFrame(history.history)
     h_df.to_csv("./output/logs/history_{}.csv".format(fname))
     print(history)
 
