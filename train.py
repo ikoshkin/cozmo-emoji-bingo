671533f21abc72e26774ebbe338f21a2b1723675
Gosha Koshkin <ihorkoshkin@gmail.com>
2018-01-22 13:56:26 -0700

    multiclass

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def save_history(history, fname):
     #             i, loss[i], acc[i], val_loss[i], val_acc[i]))
 
 
-def plot_history(hist_df):
+def plot_history(hist_df, fname='train_history.png'):
 
     acc = hist_df['acc']
     val_acc = hist_df['val_acc']
@@ -53,24 +53,24 @@ def plot_history(hist_df):
     plt.title('Training and validation loss')
     plt.legend()
 
-    plt.savefig('train_history_v0.png')
+    plt.savefig(fname)
     return
 
 labels_want = ['robot','human']
 
 def train(model_name, dataset_name):
 
-    # Data parameters
+    #: Data parameters
     data_dir = get_data_dirs(dataset_name)
     n_images = {'train': 400, 'validation': 50, 'test': 50}
     image_size = (320, 240)
 
-    # Training parameters
-    n_epochs = 20
+    #: Training parameters
+    n_epochs = 100
     batch_size = 10
     steps_per_epoch = n_images['train'] // batch_size #// n_epochs
     
-    # Load data generators
+    #: Load data generators
     
     # train_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -86,7 +86,7 @@ def train(model_name, dataset_name):
 
     test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    class_mode = 'binary'
+    class_mode = 'categorical'
 
     train_generator = train_datagen.flow_from_directory(
         data_dir['train'],
@@ -101,27 +101,20 @@ def train(model_name, dataset_name):
         class_mode=class_mode)
 
 
-    #optimizer = keras.optimizers.SGD(lr=0.1, momentum=0.9, decay= 1e-6, nesterov=True)
-    #Load model
-
+    #: Load model
     ml = ModelLoader(n_labels=2, model_name=model_name, image_size=image_size)
     model = ml.model
 
-    #Define callbacks
+    #: Define callbacks
     checkpointer = ModelCheckpoint(
         filepath='./output/hdf5/{}'.format(model_name) +
         '-{epoch:03d}-{loss:.3f}.hdf5',
         verbose=1,
         save_best_only=True)
 
-    tb = TensorBoard(log_dir='./output/logs')
-    early_stopper = EarlyStopping(patience=2)
-    csv_logger = CSVLogger('./output/logs/' + model_name + '-' + 'training-' + \
-        str(time.time()) + '.log')
+    callbacks = [checkpointer]
 
-    callbacks = [tb, early_stopper, csv_logger, checkpointer]
-
-    # Training
+    #: Training
     print('Starting training')
 
     history = model.fit_generator(
@@ -131,19 +124,24 @@ def train(model_name, dataset_name):
         verbose=1,
         callbacks=callbacks,
         validation_data=validation_generator,
-        validation_steps=10,
+        validation_steps=10
     )
 
     model.save('./output/{}.h5'.format(model_name))
     save_history(history, "hist_{}".format(model_name))
-    return
+    return history
 
 
 if __name__=='__main__':
 
-    model_name = 'vgg16_v0'
-    dataset_name = 'robot_human'
+    # model_name = 'vgg16_v0'
+    # dataset_name = 'robot_human'
 
-    train(model_name, dataset_name)
+    model_name = 'simple_cnn_multi'
+    dataset_name = 'human_robot_multiclass'
+
+    history = train(model_name, dataset_name)
+    h_df = pd.DataFrame(history.history)
     # h_df = pd.read_csv('./output-aws/logs/history_hist_small_binary_cnn_v0.csv')
-    # plot_history(h_df)
+
+    plot_history(h_df, fname='cnn_multi_h_r.png')
