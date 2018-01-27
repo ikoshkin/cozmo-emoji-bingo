ba61b1be37c5d013e54f98cf33d10d6ded656596
Gosha Koshkin <ihorkoshkin@gmail.com>
2018-01-27 05:59:48 -0700

    h3

--- hyper.py
+++ hyper.py
@@ -219,6 +219,8 @@ if __name__ == '__main__':
 
     train_datagen, val_datagen, test_datagen = data()
 
+    test_dir = './images/ten_multiclass/test'
+
     test_generator = test_datagen.flow_from_directory(
         test_dir,
         target_size=(244, 244),
@@ -241,8 +243,8 @@ if __name__ == '__main__':
                                                             timestamp=datetime.datetime.now().strftime('%m%d_%H%M%S'))
     best_model.save('./hyperas/{}_{}.h5'.format(model_name, info))
 
-    # print("Evalutation of best performing model:")
-    # print(best_model.evaluate_generator(test_generator, steps=20)) 
+    print("Evalutation of best performing model:")
+    print(best_model.evaluate_generator(test_generator, steps=20)) 
     
     if K.backend() == 'tensorflow':
             K.clear_session()
